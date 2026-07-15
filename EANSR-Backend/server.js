98ee5d9cf5b2fbe3f5032ab11a287cbc39c83291
mennafayez162 <mennafayez162@gmail.com>
2026-07-16 00:42:06 +0300

    add home route

--- EANSR-Backend/server.js
+++ EANSR-Backend/server.js
@@ -19,8 +19,14 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/articles', require('./routes/articleRoutes'));
 
+// ===== Home Route =====
+app.get('/', (req, res) => {
+    res.send('🚀 Backend is running successfully');
+});
+
 // ===== تشغيل السيرفر =====
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
